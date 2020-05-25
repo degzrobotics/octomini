6dc1d77c771c19e960913532996c9ac14ea17a3b
degzrobotics <65487555+degzrobotics@users.noreply.github.com>
2020-05-25 16:22:02 +0300

    Initial files

---
+++ controller/controller.ino
@@ -0,0 +1,142 @@
+#include <Servo.h>
+#include <SPI.h>
+#include <mcp2515.h>
+
+#define maxdeger 2000 //max 2000 oluyor escler 1000-2000 arası calisir
+#define mindeger 1000
+#define sabitleme_toleransi 30
+
+struct can_frame canSend;
+struct can_frame canRcv;
+
+MCP2515 mcp2515(8);
+
+float hizBoleni = 1.0;
+
+int pinJoyStick_X_1 = 2;
+int pinJoyStick_Y_1 = 3;
+int pinJoyStick_X_2 = 0;
+int pinJoyStick_Y_2 = 1;
+
+int valueJoyStick_X_1 = 0;
+int valueJoyStick_Y_1 = 0;
+int valueJoyStick_X_2 = 0;
+int valueJoyStick_Y_2 = 0;
+
+union ArrayToInteger {
+    byte array[2];
+    int integer;
+} converter;
+union ArrayToDouble {
+    byte array[4];
+    double number;
+} doubler;
+
+void setup()
+{
+    Serial.begin(9600);
+    SPI.begin();
+
+    mcp2515.reset();
+    mcp2515.setBitrate(CAN_125KBPS);
+    mcp2515.setNormalMode();
+/*
+    pinMode(4, OUTPUT);
+    pinMode(5, OUTPUT);
+    pinMode(8, INPUT);
+    pinMode(7, INPUT);
+    pinMode(6, INPUT);
+    pinMode(9, INPUT);
+*/
+}
+
+void loop()
+{
+    //
+    if(Serial.available())
+    {
+      int incomingByte = Serial.read();
+      hizBoleni = incomingByte / 10.0;
+    }
+    
+    // rov'dan gelen basinc ve sicaklik degerleri okunup serial'den basiliyor
+    if (mcp2515.readMessage(&canRcv) == MCP2515::ERROR_OK)
+    {
+        if (canRcv.can_id == 0x03)
+        {
+            doubler.array[0] = canRcv.data[0];
+            doubler.array[1] = canRcv.data[1];
+            doubler.array[2] = canRcv.data[2];
+            doubler.array[3] = canRcv.data[3];
+            Serial.print('b'); // basinc belirteci
+            Serial.println(doubler.number);
+            doubler.array[0] = canRcv.data[4];
+            doubler.array[1] = canRcv.data[5];
+            doubler.array[2] = canRcv.data[6];
+            doubler.array[3] = canRcv.data[7];
+            Serial.print('s'); // sicaklik belirteci
+            Serial.println(doubler.number);
+        }
+    }
+
+    // joystick'lerde ortada gelen veriyi 1500'e esitlemek icin
+    valueJoyStick_X_1 = analogRead(pinJoyStick_X_1) + 970;
+    valueJoyStick_Y_1 = analogRead(pinJoyStick_Y_1) + 970;
+    valueJoyStick_X_2 = analogRead(pinJoyStick_X_2) + 970;
+    valueJoyStick_Y_2 = analogRead(pinJoyStick_Y_2) + 970;
+
+    // hizi merkezi bozmadan bolmek icin
+    valueJoyStick_X_1 = 1500 + (valueJoyStick_X_1 - 1500) / hizBoleni;
+    valueJoyStick_Y_1 = 1500 + (valueJoyStick_Y_1 - 1500) / hizBoleni;
+    valueJoyStick_X_2 = 1500 + (valueJoyStick_X_2 - 1500) / hizBoleni;
+    valueJoyStick_Y_2 = 1500 + (valueJoyStick_Y_2 - 1500) / hizBoleni;
+    
+
+    if (valueJoyStick_X_1 > maxdeger)
+        valueJoyStick_X_1 = maxdeger;
+    if (valueJoyStick_Y_1 > maxdeger)
+        valueJoyStick_Y_1 = maxdeger;
+    if (valueJoyStick_X_2 > maxdeger)
+        valueJoyStick_X_2 = maxdeger;
+    if (valueJoyStick_X_1 < mindeger)
+        valueJoyStick_X_1 = mindeger;
+    if (valueJoyStick_Y_1 < mindeger)
+        valueJoyStick_Y_1 = mindeger;
+    if (valueJoyStick_X_2 < mindeger)
+        valueJoyStick_X_2 = mindeger;
+
+    // joystick'ler belli bi toleransla ortadayken 1500'e sabitliyoruz
+    if (valueJoyStick_X_1 < 1500 + sabitleme_toleransi / hizBoleni && valueJoyStick_X_1 > 1500 - sabitleme_toleransi / hizBoleni)
+        valueJoyStick_X_1 = 1500;
+    if (valueJoyStick_Y_1 < 1500 + sabitleme_toleransi / hizBoleni && valueJoyStick_Y_1 > 1500 - sabitleme_toleransi / hizBoleni)
+        valueJoyStick_Y_1 = 1500;
+    if (valueJoyStick_X_2 < 1470 + sabitleme_toleransi / hizBoleni && valueJoyStick_X_2 > 1470 - sabitleme_toleransi / hizBoleni)
+        valueJoyStick_X_2 = 500;
+    if (valueJoyStick_Y_2 < 1500 + sabitleme_toleransi / hizBoleni && valueJoyStick_Y_2 > 1500 - sabitleme_toleransi / hizBoleni)
+        valueJoyStick_Y_2 = 1500;
+
+    // joystick degerleri 8 byte'a siralanip yollaniyor
+    canSend.can_id = 0x02;
+    canSend.can_dlc = 8;
+    canSend.data[0] = highByte(valueJoyStick_X_1);
+    canSend.data[1] = lowByte(valueJoyStick_X_1);
+    canSend.data[2] = highByte(valueJoyStick_Y_1);
+    canSend.data[3] = lowByte(valueJoyStick_Y_1);
+    canSend.data[4] = highByte(valueJoyStick_X_2);
+    canSend.data[5] = lowByte(valueJoyStick_X_2);
+    canSend.data[6] = highByte(valueJoyStick_Y_2);
+    canSend.data[7] = lowByte(valueJoyStick_Y_2);
+
+    // q-w-e-r her degerin bilgisayardan ayirt edilmesi icin ayri kodlar
+    Serial.print('q');
+    Serial.println(valueJoyStick_X_1);
+    Serial.print('w');
+    Serial.println(valueJoyStick_Y_1);
+    Serial.print('e');
+    Serial.println(valueJoyStick_X_2);
+    Serial.print('r');
+    Serial.println(valueJoyStick_Y_2);
+
+    mcp2515.sendMessage(&canSend);
+    delay(100);
+}
---
+++ vehicle/vehicle.ino
@@ -0,0 +1,165 @@
+#include <Ethernet.h>
+
+ #include <SPI.h>
+#include <Servo.h>
+#include "mcp2515.h"
+#include <Wire.h>
+
+
+struct can_frame canMsg; // gelen mesaj
+struct can_frame canSnd; // giden mesaj
+
+MCP2515 mcp2515(8);
+
+int valueJoyStick_X_1 = 0;
+int valueJoyStick_Y_1 = 0;
+int valueJoyStick_X_2 = 0;
+int valueJoyStick_Y_2 = 0;
+
+unsigned long startMillis; //program süre sayıcısının başlangıç zaman değişkeni
+unsigned long currentMillis;
+const unsigned long period = 1000; // basınç sensörü ölçüm aralığı
+
+Servo on, arka, onsa, onso, arsa, arso;
+
+int on_deger, arka_deger, onsa_deger, onso_deger, arsa_deger, arso_deger;
+
+union ArrayToInteger {
+    byte array[2];
+    int integer;
+} converter;
+
+union ArrayToDouble {
+    byte array[4];
+    double number;
+} doubler;
+
+
+
+//Create variables to store results
+float temperature_c;
+double pressure_abs;
+
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println("Basladi.");
+    SPI.begin();
+    startMillis = millis(); // guncel zaman
+
+    mcp2515.reset();
+    mcp2515.setBitrate(CAN_125KBPS);
+    mcp2515.setNormalMode();
+
+    on.attach(6, 1000, 2000);
+    arka.attach(3, 1000, 2000);
+    onsa.attach(9, 1000, 2000);
+    onso.attach(5, 1000, 2000);
+   arsa.attach(10, 1000, 2000);
+   arso.attach(8, 1000, 2000);
+
+
+}
+
+void loop()
+{
+    currentMillis = millis();
+
+    if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK)
+    {
+    
+        Serial.print(" "); 
+    Serial.print(canMsg.can_id, HEX); // print ID
+    Serial.print(" "); 
+    Serial.print(canMsg.can_dlc, HEX); // print DLC
+    Serial.print(" ");
+    //
+    for (int i = 0; i<canMsg.can_dlc; i++)  {  // print the data
+      Serial.print(canMsg.data[i],HEX);
+      Serial.print(" ");
+    }
+        if (canMsg.can_id == 0x02)
+        {
+            for (int i = 0; i < canMsg.can_dlc; i += 2)
+            {
+                converter.array[1] = canMsg.data[i];
+                converter.array[0] = canMsg.data[i + 1];
+                if (i == 0)
+                    valueJoyStick_X_1 = converter.integer;
+                else if (i == 2)
+                    valueJoyStick_Y_1 = converter.integer;
+                else if (i == 4)
+                    valueJoyStick_X_2 = converter.integer;
+                else if (i == 6)
+                    valueJoyStick_Y_2 = converter.integer;
+            }
+
+            on.writeMicroseconds(valueJoyStick_X_1);
+            arka.writeMicroseconds(valueJoyStick_X_1);
+            onsa_deger = 1500 + (valueJoyStick_X_2 - 1500) - (valueJoyStick_Y_2 - 1500) + (valueJoyStick_Y_1 - 1500);
+            onso_deger = 1500 + (valueJoyStick_X_2 - 1500) + (valueJoyStick_Y_2 - 1500) - (valueJoyStick_Y_1 - 1500);
+            arsa_deger = 1500 + (valueJoyStick_X_2 - 1500) + (valueJoyStick_Y_2 - 1500) + (valueJoyStick_Y_1 - 1500);
+            arso_deger = 3000-(1500 + (valueJoyStick_X_2 - 1500) - (valueJoyStick_Y_2 - 1500) - (valueJoyStick_Y_1 - 1500));
+
+            Serial.println(onsa_deger);
+            Serial.println(onso_deger);
+            Serial.println(arsa_deger);
+            Serial.println(arso_deger);
+            
+            if (onsa_deger >= 2000)
+                onsa_deger = 2000;
+            else if (onsa_deger <= 1000)
+                onsa_deger = 1000;
+            if (arsa_deger >= 2000)
+                arsa_deger = 2000;
+            else if (arsa_deger <= 1000)
+                onsa_deger = 1000;
+            if (onso_deger >= 2000)
+                onso_deger = 2000;
+            else if (onso_deger <= 1000)
+                onsa_deger = 1000;
+            if (arso_deger >= 2000)
+                arso_deger = 2000;
+            else if (arso_deger <= 1000)
+                onsa_deger = 1000;
+              onsa.writeMicroseconds(onsa_deger);
+              onso.writeMicroseconds(onso_deger);
+              arsa.writeMicroseconds(arsa_deger);
+              arso.writeMicroseconds(arso_deger);
+            delay(5);
+            
+            Serial.print(valueJoyStick_X_1);
+            Serial.print("--");
+            Serial.print(valueJoyStick_Y_1);
+            Serial.print("--");
+            Serial.print(valueJoyStick_X_2);
+            Serial.print("--");
+            Serial.print(valueJoyStick_Y_2);
+            Serial.println("--"); 
+        }
+    }
+    
+    /*if (currentMillis - startMillis >= period) //test whether the period has elapsed
+    {
+
+
+        Serial.println(pressure_abs);
+        Serial.println(temperature_c);
+
+        canSnd.can_id = 0x03;
+        canSnd.can_dlc = 8;
+        doubler.number = pressure_abs;
+        canSnd.data[0] = doubler.array[0];
+        canSnd.data[1] = doubler.array[1];
+        canSnd.data[2] = doubler.array[2];
+        canSnd.data[3] = doubler.array[3];
+        doubler.number = temperature_c;
+        canSnd.data[4] = doubler.array[0];
+        canSnd.data[5] = doubler.array[1];
+        canSnd.data[6] = doubler.array[2];
+        canSnd.data[7] = doubler.array[3];
+
+        startMillis = currentMillis; //IMPORTANT to save the start time of the current LED state.
+        mcp2515.sendMessage(&canSnd);
+    }*/
+}
